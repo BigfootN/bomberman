6a3d08b6557ed27624cdb3c468d9719acafd03da
BigfootN <bigfoot_arch@protonmail.com>
2018-03-08 13:16:59 +0100

    update gitignore

--- src/introduction/introloop.h
+++
@@ -1,9 +0,0 @@
-#ifndef INTRO_LOOP_H
-#define INTRO_LOOP_H
-
-#include "types.h"
-
-int choice_menu(control_t*, SDL_event);
-int events_loop_introduction(control_t*, int*);
-
-#endif
--- src/introduction/introviews.h
+++
@@ -1,8 +0,0 @@
-#ifndef SYMBOL
-#define SYMBOL
-
-#include "types.h"
-
-void sprite_panel_introduction(control_t* control);
-
-#endif
