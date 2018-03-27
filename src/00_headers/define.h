29beebd4a86b975c8b95f0023ff808d3b2c1fc69
BigfootN <bigfoot_arch@protonmail.com>
2018-03-27 20:51:32 +0200

    update

--- src/00_headers/define.h
+++ src/00_headers/define.h
@@ -9,30 +9,30 @@
 
 // fenetre principale
 #define WINDOW_WIDTH 840
-#define WINDOW_HEIGHT 640 //473
+#define WINDOW_HEIGHT 640	//473
 
 // string
 #define STRING_BLOCK_SIZE 1024
-#define SGN(X) (((X)==0)?(0):(((X)<0)?(-1):(1)))
-#define ABS(X) ((((X)<0)?(-(X)):(X)))
+#define SGN(X) (((X) == 0) ? (0) : (((X) < 0) ? (-1) : (1)))
+#define ABS(X) ((((X) < 0) ? (-(X)) : (X)))
 
 // le mesuration de chaque titles
 #define HEIGHT_TILE 40
 #define WIDTH_TILE 40
 
-//#if defined WIN32, defined WIN64
+#if defined (WIN32) || defined(WIN64)
 #define REP_SPRITES "res\\sprites\\"
+#define REP_MAP "res\\map\\"
+#define REP_TTF "res\\roboto\\"
+#elif defined __linux__
+#define REP_SPRITES "res/sprites/"
 #define REP_MAP "res/map/"
 #define REP_TTF "res/roboto/"
-//#elif defined __linux__
-//#define REP_SPRITES "res/sprites/"
-//#define REP_MAP "res/map/"
-//#define REP_TTF "res/roboto/"
-//#endif
+#endif
 
 #define SIGNALWHATEVER 45
 
 #define STEP 3
 #define STEPTIME 3
 
-#endif //BOMBERMAN_DEFINE_H
+#endif	//BOMBERMAN_DEFINE_H
